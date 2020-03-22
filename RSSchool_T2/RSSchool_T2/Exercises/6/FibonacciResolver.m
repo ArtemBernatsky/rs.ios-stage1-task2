75904b63911ca5487f24a0b0052f75fe4c188ca3
Artem Bernatsky <bernatskyhome@gmail.com>
2020-03-22 15:59:21 +0300

    feat: add 6 task

--- RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
+++ RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
@@ -2,6 +2,24 @@
 
 @implementation FibonacciResolver
 - (NSArray*)productFibonacciSequenceFor:(NSNumber*)number {
-    return @[];
+    int n = [number intValue];
+    NSMutableArray *array = [[NSMutableArray alloc] init];
+
+    int firstEl = 0;
+    int currentEl = 1;
+
+    while (currentEl < n) {
+        int sum = firstEl + currentEl;
+        firstEl = currentEl;
+        currentEl  = sum;
+        if (firstEl * currentEl == n) {
+            array = [NSMutableArray arrayWithObjects:@(firstEl), @(currentEl), @1, nil];
+            break;
+        } else if (firstEl * currentEl > n) {
+            array = [NSMutableArray arrayWithObjects:@(firstEl), @(currentEl), @0, nil];
+            break;
+        }
+    }
+    return array;
 }
 @end
