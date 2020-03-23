67f83e5e071bedd0d06dbf5f2cf0dc30a8492e40
Artem Bernatsky <bernatskyhome@gmail.com>
2020-03-23 19:53:48 +0300

    feat: task 5

--- RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
+++ RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
@@ -5,19 +5,28 @@
 - (instancetype)init {
     self = [super init];
     if (self) {
-
-        _blockA = ^(NSArray *array) {
-
+        self.blockA = ^(NSArray *array) {
+            self.array = array;
         };
+        self.blockB = ^(Class class) {
+            NSMutableArray *tempArray = [self.array mutableCopy];
+            [tempArray removeObjectIdenticalTo: [NSNull null]];
+            [tempArray filterUsingPredicate: [NSPredicate predicateWithFormat: @"self isKindOfClass: %@", class]];
 
-        _blockB = ^(Class class) {
-
+            if (class == [NSString class]) {
+                self.blockC([tempArray componentsJoinedByString: @""]);
+            } else if (class == [NSNumber class]) {
+                self.blockC([tempArray valueForKeyPath: @"@sum.self"]);
+            } else if (class == [NSDate class]) {
+                NSDate *neededDate = [tempArray firstObject];
+                for (NSDate *currentDate in tempArray) {
+                    neededDate = [currentDate laterDate: neededDate];
+                }
+                NSDateFormatter *format = [[NSDateFormatter alloc] init];
+                [format setDateFormat: @"dd.MM.yyyy"];
+                self.blockC([format stringFromDate: neededDate]);
+            }
         };
-
-        _blockC = ^(NSObject *object) {
-
-        };
-
     }
     return self;
 }
