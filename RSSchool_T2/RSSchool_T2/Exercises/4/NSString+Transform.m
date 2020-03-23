b5d058cbff6e76075cc30858ec96afe271156ab1
Artem Bernatsky <bernatskyhome@gmail.com>
2020-03-23 19:44:51 +0300

    feat: task 4

--- RSSchool_T2/RSSchool_T2/Exercises/4/NSString+Transform.m
+++ RSSchool_T2/RSSchool_T2/Exercises/4/NSString+Transform.m
@@ -3,7 +3,60 @@
 @implementation NSString (Transform)
 
 -(NSString*)transform {
-    return self;
+    NSCharacterSet *vowels = [NSCharacterSet characterSetWithCharactersInString: @"AEIOUYaeiouy"];
+    NSCharacterSet *consonants = [NSCharacterSet characterSetWithCharactersInString: @"BCDFGHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz"];
+    NSCharacterSet *characterSetForSearch = [self isPangram] ? vowels : consonants;
+    
+    NSMutableArray<NSString*> *words = [NSMutableArray arrayWithArray: [self componentsSeparatedByString:@" "]];
+    NSMutableArray<NSString*> *result = [[NSMutableArray alloc] init];
+    
+    for (NSString *word in words) {
+        if (![word isEqualToString:@""]) {
+            NSMutableString *mutableWord = [NSMutableString stringWithString: word];
+            int count = 0;
+            
+            for (int i = 0; i < word.length; i++) {
+                NSRange range = NSMakeRange(i, 1);
+                NSString *currentCharacter = [word substringWithRange: range];
+                if ([currentCharacter rangeOfCharacterFromSet: characterSetForSearch].location != NSNotFound) {
+                    [mutableWord replaceCharactersInRange: range withString: [currentCharacter uppercaseString]];
+                    count++;
+                }
+            }
+            [result addObject: [NSString stringWithFormat:@"%d%@", count, mutableWord]];
+        }
+    }
+    
+    [result sortUsingComparator: ^NSComparisonResult(id word1, id word2) {
+        NSRange weightCursor = NSMakeRange(0, 1);
+        
+        int firstWeight = [[word1 substringWithRange: weightCursor] intValue];
+        int secondWeight = [[word2 substringWithRange: weightCursor] intValue];
+        
+        if (firstWeight < secondWeight) {
+            return NSOrderedAscending;
+        } else if (firstWeight == secondWeight) {
+            return NSOrderedSame;
+        } else {
+            return NSOrderedDescending;
+        }
+    }];
+    
+    return [result componentsJoinedByString:@" "];
+}
+
+- (BOOL)isPangram {
+    NSArray<NSString*>* alphabet = @[@"A", @"B", @"C", @"D", @"E", @"F", @"G", @"H", @"I", @"J", @"K", @"L", @"M",
+                                     @"N", @"O", @"P", @"Q", @"R", @"S", @"T", @"U", @"V", @"W", @"X", @"Y", @"Z"];
+    
+    NSMutableSet<NSString*>* uniqCharacters = [[NSMutableSet alloc] initWithArray: alphabet];
+    
+    for (int i = 0; i < self.length; i++) {
+        NSString *currentCharacter = [self substringWithRange: NSMakeRange(i, 1)];
+        [uniqCharacters removeObject: [currentCharacter uppercaseString]];
+    }
+    
+    return [uniqCharacters count] == 0;
 }
 
 @end
